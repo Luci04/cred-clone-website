a24552d2637cca04e7ef464ef3e8117db39cc59f
luci04 <avinashukla2000@gmail.com>
2022-06-20 04:17:11 +0530

    Hero-section

--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './heroSection.css'
+import Button from '../common/Button'
 
 function HeroSection() {
     return (
         <div className='hero-section-wrapper'>
-            <div className='flex absolute-center'>
-                Aviansh
+            <div className='flex absolute-center hero-claim-label' >
+                Pay credit card bill. earn guranteed ₹200 back.
+                <div className='claim-anchor'>
+                    claim-now <img src="https://web-images.credcdn.in/_next/assets/images/home-page/arrow.png" alt="cred logo" className='claim-arrow' />
+                </div>
+            </div>
+
+            <div className='flex absolute-center flex-col hero-section max-width'>
+                <div className='hero-heading'>
+                    reward for paying cards bills.
+                </div>
+                <div className='hero-subheading'>
+                    join 7.5M+ members who win rewards and cashbacks everyday
+                </div>
+                <Button buttonText="Download CRED" />
             </div>
         </div>
     )
